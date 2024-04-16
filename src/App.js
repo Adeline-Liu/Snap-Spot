ae7f68fa17155aa0c44a5a25f08f296a3d799969
Adeline-Liu <yifei05@outlook.com>
2024-04-15 22:07:11 -0700

    Added user components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
 import './App.css';
 
 const App = () => {
-  return <Router></Router>;
+  return <Router>
+    <Switch>
+    {/* Switch looks through all its child Route components and render the first one matches the current URL  */}
+    <Route path = "/" exact = {true}>
+      {/* this Route will only be rendered if the path is exactly '/' */}
+      <Users/>
+    </Route>
+    <Route path = "/places/new" exact = {true}>
+      <NewPlace/>
+    </Route>
+    <Redirect to = "/"/>
+    {/* if the path is not '/', redirect component will redirect the page to '/' */}
+    </Switch>
+  </Router>;
 };
 
 export default App;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+    const USERS = [{id: 'user1', name: 'Adeline', 
+    image: 'https://www.iconpacks.net/free-icon/no-profile-picture-15257.html', 
+    places: 3}];
 
+    return <UsersList items={USERS}/>;
 };
 
 export default Users;
