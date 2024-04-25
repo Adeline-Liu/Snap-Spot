18dc8e2cce8ca3e800448450a78982456cdd1f84
Adeline-Liu <yifei05@outlook.com>
2024-04-24 22:22:22 -0700

    Added side drawer component with open and close functionalities

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
+
 import Mainheader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+    // console.log("button pressed!");
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     // only one root per component, so wrapped by <React.Fragment> to fulfill this requirement
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
-      <Mainheader className="main-navigation__menu-btn">
-        <button>
+      {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
+      {/* onece the drawer is open, render the backdrop component in the background to close the drawer for future use */}
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
+      <Mainheader className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button onClick={openDrawer}>
+          {/* this button will trigger the drawer to open */}
           <span />
           <span />
           <span />
