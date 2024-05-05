ff157f520d6b4c7ad99a6b414f433d05bfcce385
Adeline-Liu <yifei05@outlook.com>
2024-05-05 15:12:40 -0700

    Updated Modal

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -8,6 +8,9 @@ import { CSSTransition } from "react-transition-group";
 // internal use only, so don't need to export, will used by modal
 const ModalOverlay = (props) => {
   const content = (
+    // the ${} syntax is used inside backticks (``) to insert a JavaScript expression into a string
+    // in this case, props.className, props.style, props.headerClass, props.contentClass, props.children, props.footer
+    // we do that to complete the class name, and maybe something else
     <div className={`modal ${props.className}`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
         <h2>{props.header}</h2>
@@ -15,6 +18,8 @@ const ModalOverlay = (props) => {
       <form
         onSubmit={
           props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+          // if props.onSubmit is not defined, event.preventDefault()
+          // is used to prevent the form from being submitted and the page from reloading
         }
       >
         <div className={`modal__content ${props.contentClass}`}>
@@ -33,11 +38,13 @@ const Modal = (props) => {
   return (
     <React.Fragment>
       {props.show && <Backdrop onClick={props.onCancel} />}
+      {/* conditionally renders the Backdrop component if the show prop is true
+      the Backdrop component is only rendered when the modal is visible */}
       <CSSTransition
-        in={props.show}
+        in={props.show} // when props.show is true (when showMap is true), the animation will be rendered
         mountOnEnter
         unmountOnExit
-        timeout={200}
+        timeout={280}
         classNames="modal"
       >
         <ModalOverlay {...props} />
