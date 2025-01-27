b999e4cf1a85d34e54d1ef748d2d4c787592b592
Adeline-Liu <yifei05@outlook.com>
2025-01-26 23:15:26 -0800

    Implemented authentication context and conditional routing for logged-in users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,39 +12,94 @@ import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 import "./App.css";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
-    <Router>
-      {/* Router wraps the entire application, enabling routing across the app 
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      {/* AuthContext.Provider is a component that wraps the entire application
+    and provides the context to all its children, in this case, 
+    AuthContext makes sure some features are for authenticated users only,
+    and some are for unauthenticated users only. */}
+      <Router>
+        {/* Router wraps the entire application, enabling routing across the app 
       based on links clicked or paths navigated to by the user */}
-      <MainNavigation />
-      <main>
-        <Switch>
+        <MainNavigation />
+        <main>
+          {routes}
           {/* Switch looks through all its child Route components and render the first one matches the current URL  */}
-          <Route path="/" exact={true}>
-            {/* this Route will only be rendered if the path is exactly '/' */}
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact={true}>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Redirect to="/" />
+          {/* <Route path="/" exact={true}> */}
+          {/* this Route will only be rendered if the path is exactly '/' */}
+          {/* <Users />
+            </Route>
+            <Route path="/:userId/places" exact>
+              <UserPlaces />
+            </Route>
+            <Route path="/places/new" exact={true}>
+              <NewPlace />
+            </Route>
+            <Route path="/places/:placeId">
+              <UpdatePlace />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Redirect to="/" /> */}
           {/* if the path is not '/', redirect component will redirect the page to '/' */}
-        </Switch>
-      </main>
-    </Router>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
