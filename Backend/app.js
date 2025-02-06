6cf7a6d19d254800c8d4c6df19a0e4e6487551f9
Adeline-Liu <yifei05@outlook.com>
2025-02-06 00:46:34 -0800

    Added error handling and implemented routes to fetch places by ID

--- Backend/app.js
+++ Backend/app.js
@@ -9,19 +9,15 @@ const app = express(); // create an Express application instance
 // res (response) → The response object to send data back to the client
 // next() → Calls the next middleware in the stack
 
-// app.use(bodyParser.urlencoded({ extended: false })); // parse all incoming requests if are urlencoded data
+app.use("/api/places", placesRoutes); // use placesRoutes as middleware, all routes inside placesRoutes are prefixed with /api/places
 
-// app.post("/user", (req, res, next) => {
-//   res.send("<h1>User: " + req.body.username + "</h1>");
-// });
-
-// // for request to this URL "/", this function will be executed
-// app.get("/", (req, res, next) => {
-//   res.send(
-//     '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form>'
-//   );
-// });
-
-app.use(placesRoutes); // use placesRoutes as middleware
+app.use((error, req, res, next) => {
+  // this middleware function will be executed for any request that has an error
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 app.listen(5001);
--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -2,9 +2,50 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Reuqest in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    address: "20 W 34th St, New York, NY 10118, United States",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  // :pid is a route parameter, so Express will extract whatever value appears in that position of the URL
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided ID :(");
+    error.code = 404;
+    return next(error); // this will be caught by the error handling middleware
+  }
+
+  res.json({ place }); // { place } is the same as { place: place } if the key and the value have the same name
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    const error = new Error(
+      "Could not find a place for the provided user ID :("
+    );
+    error.code = 404;
+    return next(error); // this will be caught by the error handling middleware
+  }
+  res.json({ place });
 });
 
 module.exports = router;
