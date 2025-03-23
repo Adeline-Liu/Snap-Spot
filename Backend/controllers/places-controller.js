7dc11f3a459000f6338346d48d372fe0a4746038
Adeline-Liu <yifei05@outlook.com>
2025-03-23 10:49:00 -0700

    Updated getPlacesByUserId and related logic to return multiple places for a user

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -31,18 +31,18 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided user ID :(", 404)
     ); // this will be caught by the error handling middleware with parameter error
   }
-  res.json({ place });
+  res.json({ places });
 };
 
 // get requests don't have a body, but post requests do
@@ -89,7 +89,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
